d0769d36deeeac8cb703b9d1ead12122f4384ef4
Neil Shen <overvenus@gmail.com>
2018-10-16 19:01:12 +0800

    Panic if key is not in region

--- src/raftstore/store/region_snapshot.rs
+++ src/raftstore/store/region_snapshot.rs
@@ -132,13 +132,17 @@ impl Clone for RegionSnapshot {
 
 impl Peekable for RegionSnapshot {
     fn get_value(&self, key: &[u8]) -> Result<Option<DBVector>> {
-        util::check_key_in_region(key, &self.region)?;
+        if let (e) = util::check_key_in_region(key, &self.region) {
+            panic!("key not in region key: {:?}, region: {:?}", key, self.region);
+        }
         let data_key = keys::data_key(key);
         self.snap.get_value(&data_key)
     }
 
     fn get_value_cf(&self, cf: &str, key: &[u8]) -> Result<Option<DBVector>> {
-        util::check_key_in_region(key, &self.region)?;
+        if let (e) = util::check_key_in_region(key, &self.region) {
+            panic!("key not in region key: {:?}, region: {:?}", key, self.region);
+        }
         let data_key = keys::data_key(key);
         self.snap.get_value_cf(cf, &data_key)
     }
@@ -306,7 +310,10 @@ impl RegionIterator {
 
     #[inline]
     pub fn should_seekable(&self, key: &[u8]) -> Result<()> {
-        util::check_key_in_region_inclusive(key, &self.region)
+        if let (e) = util::check_key_in_region_inclusive(key, &self.region) {
+            panic!("key not in region key: {:?}, region: {:?}", key, self.region);
+        }
+        Ok(())
     }
 }
 
