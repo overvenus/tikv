2a6f6bdf20a40c7d6bcf84e12120a2b0885d5709
Neil Shen <overvenus@gmail.com>
2020-03-11 19:39:13 +0800

    encryption: add encryption cli
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- components/encryption/src/lib.rs
+++ components/encryption/src/lib.rs
@@ -15,9 +15,10 @@ mod manager;
 mod master_key;
 mod metadata;
 
+pub use self::config::*;
 pub use self::crypter::{AesCtrCrypter, Iv};
 pub use self::encrypted_file::EncryptedFile;
 pub use self::errors::{Error, Result};
 pub use self::manager::DataKeyManager;
-pub use self::master_key::{FileBackend, KmsBackend};
+pub use self::master_key::{Backend, FileBackend, KmsBackend};
 pub use self::metadata::*;
